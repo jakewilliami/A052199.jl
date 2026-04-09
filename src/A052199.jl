ca6f162ddb4640bd88f13a1418e31d539934576b
Jake Ireland <jakewilliami@icloud.com>
2026-04-09 21:45:44 +1200

    Improve number of square sums algorithm!  v1.0.1
    An important fact of this package is that A052199 is a subsequence of
    A001481: "numbers that are the sum of 2 squares."  Moreover, A052199
    filters A001481 by counting how many ways of representing a number as
    the sum of two squares there are.
    
    This counting of the number of ways of representing the number as the
    sum of two squares is crucial to the computation of terms in the
    sequence.
    
    Previously, I had implemented the naïve approach for this function.
    Luckily, after a bit of research, some clever mathematicians have
    already devised methods of calculating the answer using prime numbers.
    
    The speed-up is ∼800× faster than the previous function.  Now, I need
    to see if I can speed up the iterator itself.  Unfortunately, it is a
    fast-growing sequence so higher terms are more difficult to compute.

--- src/A052199.jl
+++ src/A052199.jl
@@ -1,5 +1,7 @@
 module A052199
 
+using Primes
+
 export a052199, l052199
 
 ### Iteration Interface ###
@@ -15,16 +17,105 @@ a052199(T::Type{<:Integer} = Int) = _Iterator{T}()
 
 # Internal state struct for iteration interface
 struct _State{T <: Integer}
-    a::T    # The current best number by ways you can write as sum of two squares (TODO: should I use aₙ?)
-    r::Int  # The number of ways you can write `n` as the sum of two squares (r for representations)
+    a::T    # The current best number, rated by ways you can write as sum of two squares
+    r::Int  # The number of ways you can write a as the sum of two squares (r for representations)
 end
 
+# This function counts the number of distinct ways that an integer a can be written as the
+# sum of two squares, x² + y².
+#
+# That is (given our previous notation), this function computes r(a).
+#
+# Previous implementation:
+#   <https://github.com/jakewilliami/A052199.jl/blob/v1.0.0/src/A052199.jl#L22-L28>
 function _number_of_two_square_sums(a::Int)
-    return sum(1:isqrt(a), init = 0) do i
-        sum(1:(i - 1), init = 0) do j
-            return i^2 + j^2 == a
+    # Any integer can be represented as prime factors.  We need this number's factorisation
+    #
+    # <https://en.wikipedia.org/wiki/Fundamental_theorem_of_arithmetic>
+    factors = factor(a)
+
+    # Check primes ≡ 3 (mod 4)
+    # ========================
+    #
+    # The sum of two squares theorem states that
+    #     An integer greater than one can be written as a sum of two squares ⟺ if its
+    #     prime decomposition contains no factor pᵏ, where prime p ≡ 3 (mod 4) and k is
+    #     odd.
+    #
+    # Therefore, step 1 is to elimitate the case that _no_ squares exist.
+    #
+    #   <https://en.wikipedia.org/wiki/Sum_of_two_squares_theorem>
+    for (p, k) in factors
+        if mod(p, 4) == 3 && isodd(k)
+            return 0
         end
     end
+
+    # Compute product over primes ≡ 1 (mod 4)
+    # =======================================
+    #
+    # There are eight ways of writing equivalent solutions of sums of squares.  For
+    # example, x² + y² = (−x)² + (−y)² = (−y)² + x².
+    #
+    # Therefore, if we can count the set of all integer pairs whose sum of squares
+    # gets our number:
+    #     |{(x, y) ∈ ℤ²: x² + y² = n}|
+    #
+    # Then, if we can find the total number of representations r(n), we can simply
+    # divide it by eight.
+    #
+    # By the fundamental theorem of arithmetic, any number n can be written as the product
+    # of primes
+    #     n = p₁^(α_p₁) ⋅ p₂^(α_p₂) ⋅ … = Π p^(α_p)
+    #
+    # But Jacobi's two-square theorem states that the number of ways of writing n as a
+    # sum of two squares is:
+    #     r(n) = 4 Π (α_p + 1)      for all p ≡ 1 (mod 4)
+    #
+    # This behaves like a restricted divisor count.  We have already filtered out odd
+    # exponents from our previous step, so
+    #
+    #
+    # To find the number of ways a positive integer n > 1 can be expressed as a sum of
+    # 2 squares, ignoring order and signs, factor it as
+    #     n = 2^a₀ + p₁^(2a₁) ⋯ pᵣ^(2aᵣ) q₁^(b₁) ⋯ q_s^(b_s)
+    #
+    # Where pᵢ are primes of the form 4k + 3 (i.e., pᵢ ≡ 3 (mod 4))
+    # And qᵢ are primes of the form 4k + 1 (i.e, qᵢ ≡ 1 (mod 4))
+    #
+    # Because we know that n does not have such a represesentation with integer aᵢ (because
+    # one or more of the powers of pᵢ is odd, if we got here in the function), then we
+    # define:
+    #     B ≡ (b₁ + 1) (b₂ + 1) ⋯ (bᵣ + 1)
+    #
+    # Remember, each b is the exponent of all primes qᵢ ≡ 1 (mod 4).
+    #
+    # Then, the number of representations of n as the sum of two squares, ignoring order
+    # and signs, for n > 1, is then given by
+    #     r(n) = 4B
+    #
+    # Restricting to distinct positive solutions with x > y > 0, each solution is counted
+    # 8 times in r(n), so the number of such solutions is:
+    #     r(n) = 4B / 8
+    #          = B / 2
+    #
+    # This division will remove duplicates.
+    #
+    #   <https://mathworld.wolfram.com/SumofSquaresFunction.html>
+    #
+    # Note: Jacobi's theorem similarly states that:
+    #     r(n) = 4(d₁(n) - d₃(n))
+    #
+    # Where:
+    #   d₁(n) is number of divisors of a congruent to 1 (mod 4)
+    #   d₃(n) is number of divisors of a congruent to 3 (mod 4)
+    #
+    #   <https://en.wikipedia.org/wiki/Sum_of_two_squares_theorem#Jacobi's_two-square_theorem>
+    r = prod(factors, init = 1) do (p, k)
+        isone(mod(p, 4)) ? k + 1 : 1
+    end
+
+    return r ÷ 2  # Equivalent to r(n) ÷ 8
 end
 
 # Base case: 1 is the first positive number and can be written as the sum of two squares
